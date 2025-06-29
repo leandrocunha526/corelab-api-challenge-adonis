663092a3bc25521c1e0e1933e7cb9766171e83a2
Leandro Cunha <leandrocunha016@gmail.com>
2025-06-29 11:51:06 -0300

    feat: enhance user and task validators with regex and length constraints

--- app/validators/store_task.ts
+++ app/validators/store_task.ts
@@ -2,8 +2,8 @@ import vine, { SimpleMessagesProvider } from '@vinejs/vine'
 
 export const storeTaskValidator = vine.compile(
   vine.object({
-    title: vine.string().trim().minLength(3).maxLength(100),
-    text: vine.string().minLength(5).maxLength(1000),
+    title: vine.string().trim().minLength(3).maxLength(255),
+    text: vine.string().minLength(5).maxLength(256),
     color: vine.string().trim().regex(/^#([A-Fa-f0-9]{3}|[A-Fa-f0-9]{6})$/).optional(), // The hexadecimal color code is at least 3 characters long as #f00 and at most 6 characters long as #ffffff
     isFavorite: vine.boolean(),
   })
